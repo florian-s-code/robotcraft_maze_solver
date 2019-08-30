087afc3edf8cc3f1f9b62a9fd4c92df792c995de
Florian S <florian-s.github@mailo.com>
2019-08-30 17:21:32 +0100

    Redo algorithm

--- src/WPA/WPA.cpp
+++ src/WPA/WPA.cpp
@@ -9,18 +9,88 @@
 //DEBUG
 #include <iostream>
 
+void WavefrontPlanner::flipOccupancyGrid() {
+    std::vector<int8_t> flipped_grid;
 
-int8_t WavefrontPlanner::getOccupancyValue(WavefrontPlanner::Coord cell)
+    m_occupancy_grid.push_back(-100); // we add an element to occupancy grid to be able to select last
+
+    for(int i = m_height-1; i > -1; i--) {
+        //std::cout << "Flipping line " << i << std::endl;
+	flipped_grid.insert(flipped_grid.end(), &m_occupancy_grid.at(i*m_width), &m_occupancy_grid.at((i+1)*m_width));
+    }
+
+
+/*//DEBUG
+#define LINE_LENGTH 63
+    for (int i = 0; i < m_width * m_height; i++)
+    {
+        if((i+1)/m_width > LINE_LENGTH) {
+            std::cout << std::endl;
+            break;
+        }
+
+        if((i+1)%m_width > LINE_LENGTH) {
+            if( (i+1)%m_width == LINE_LENGTH )
+                std::cout << std::endl;
+            continue;
+        }
+
+        if (flipped_grid[i] < 10)
+            std::cout << static_cast<int>(flipped_grid[i]) << "  ";
+        else if (flipped_grid[i] == 100)
+            std::cout << 1 << "  ";
+        else
+            std::cout << static_cast<int>(flipped_grid[i]) << " ";
+
+        if ((i + 1) % m_width == 0)
+            std::cout << std::endl;
+    }
+//*/
+    m_occupancy_grid = flipped_grid;
+}
+
+int8_t WavefrontPlanner::getOccupancyValue(WavefrontPlanner::Cell cell)
 {
     return m_occupancy_grid[cell.y * m_width + cell.x];
 }
 
-int WavefrontPlanner::getCellValue(WavefrontPlanner::Coord cell)
+int WavefrontPlanner::getCellValue(WavefrontPlanner::Cell cell)
 {
     return m_WPA_map[cell.y * m_width + cell.x];
 }
 
-bool WavefrontPlanner::isPathableCell(WavefrontPlanner::Coord cell)
+WavefrontPlanner::Coord WavefrontPlanner::mapToOdom(WavefrontPlanner::Coord coord)
+{
+    WavefrontPlanner::Coord new_coord;
+    new_coord.x = coord.x + m_origin.x;
+    new_coord.y = -coord.y + m_origin.y + m_height*m_resolution;
+    return new_coord;
+}
+/* Convert a coordinate in meters into a int coordinate in cell */
+WavefrontPlanner::Cell WavefrontPlanner::coordToCell(WavefrontPlanner::Coord coord)
+{
+    WavefrontPlanner::Cell new_coord;
+    new_coord.x = static_cast<int>(coord.x/m_resolution);
+    new_coord.y = static_cast<int>(coord.y/m_resolution);
+    return new_coord;
+}
+
+WavefrontPlanner::Coord WavefrontPlanner::cellToCord(WavefrontPlanner::Cell cell)
+{
+    WavefrontPlanner::Coord new_coord;
+    new_coord.x = cell.x*m_resolution;
+    new_coord.y = cell.y*m_resolution;
+    return new_coord;
+}
+WavefrontPlanner::Coord WavefrontPlanner::odomToMap(WavefrontPlanner::Coord coord) 
+{
+    WavefrontPlanner::Coord new_coord;
+    new_coord.x = coord.x - m_origin.x;
+    new_coord.y = -(coord.y - m_origin.y - m_height*m_resolution);
+    return new_coord;
+}
+
+bool WavefrontPlanner::isPathableCell(WavefrontPlanner::Cell cell)
 {
     // out-of-bound check
     if (!(cell.x >= 0 && cell.y >= 0 && cell.x < m_height && cell.y < m_width))
@@ -30,19 +100,24 @@ bool WavefrontPlanner::isPathableCell(WavefrontPlanner::Coord cell)
     return (getOccupancyValue(cell) == 0);
 }
 
-std::vector<WavefrontPlanner::Coord> WavefrontPlanner::getAdjacentCoords(const WavefrontPlanner::Coord& cell)
+std::vector<WavefrontPlanner::Cell> WavefrontPlanner::getAdjacentCoords(const WavefrontPlanner::Cell& cell)
 {
-    std::vector<WavefrontPlanner::Coord> coords;
+    std::vector<WavefrontPlanner::Cell> coords;
+
+    if(cell.x == 1 && cell.y == 39)
+        std::cout << "Adjacent occupancy of (" << cell.x << ", " << cell.y <<"), value : " << getCellValue(cell) << " :" << std::endl;
 
     // iterate over every adjacent coord and add them to list if
     // - it is not out of bound
     // - it is traversable (occupancy value == 0)
-    // - they don't already have a value
     for (int i = 0; i < 4; i++)
     {
-        WavefrontPlanner::Coord next_coord;
+        WavefrontPlanner::Cell next_coord;
         next_coord.x = cell.x + A[i].x;
         next_coord.y = cell.y + A[i].y;
+
+        if(cell.x == 1 && cell.y == 39)
+             std::cout << static_cast<int>(getCellValue(next_coord)) << std::endl;
 
         if (isPathableCell(next_coord) ) //check bound and occupancy
         {
@@ -60,13 +135,14 @@ void WavefrontPlanner::getCoordFromIndex(int cell_index, int& x, int& y)
     y = cell_index / m_width;
 }
 
-void WavefrontPlanner::setCellValue(WavefrontPlanner::Coord cell, int value)
+void WavefrontPlanner::setCellValue(WavefrontPlanner::Cell cell, int value)
 {
     m_WPA_map[cell.y * m_width + cell.x] = value;
 }
 
-WavefrontPlanner::WavefrontPlanner(int width, int height, const std::vector<int8_t>& occupancy_grid)
-  : m_width(width), m_height(height)
+//constructor
+WavefrontPlanner::WavefrontPlanner(int width, int height, float origin_x, float origin_y, float resolution, const std::vector<int8_t>& occupancy_grid)
+  : m_width( width ), m_height( height ), m_resolution(resolution)
 {
     // copy the occupancy_grid into the object
     m_occupancy_grid = std::vector<int8_t>(occupancy_grid.begin(), occupancy_grid.end());
@@ -90,36 +166,57 @@ WavefrontPlanner::WavefrontPlanner(int width, int height, const std::vector<int8
     m_end_cell.x = m_width - 1;
     m_end_cell.y = m_height - 1;
 
+    m_origin = { origin_x, origin_y };
+
+    flipOccupancyGrid();
+}
+
+void WavefrontPlanner::init() {
     createMap();
     createPath();
 }
-
-void WavefrontPlanner::setStart(int x, int y)
+/*Set starting position to the position (in odom coordinate plane)*/
+void WavefrontPlanner::setStart(float x, float y)
 {
-    m_start_cell.x = x;
-    m_start_cell.y = y;
+    WavefrontPlanner::Cell position = coordToCell( odomToMap( { x, y } ) );
+    m_start_cell = position;
+
+    std::cout << "Start x: " << position.x << ", y: " << position.y << std::endl;
 }
 
-void WavefrontPlanner::setEnd(int x, int y)
+/*Set end position to the position (in odom coordinate plane)*/
+void WavefrontPlanner::setEnd(float x, float y)
 {
-    m_end_cell.x = x;
-    m_end_cell.y = y;
+    WavefrontPlanner::Cell position = coordToCell( odomToMap( { x, y } ) );
+    m_end_cell = position;
+
+    std::cout << "End x: " << position.x << ", y: " << position.y << std::endl;
 }
 
 void WavefrontPlanner::createMap()
 {
+
+    m_WPA_map = std::vector<int>(m_width * m_height, 0);
+
     // set end cell to 1
     setCellValue(m_end_cell, 1);
-    std::deque<WavefrontPlanner::Coord> cells_to_check = { m_end_cell };  // store cells to check, initialized to end
+    std::deque<WavefrontPlanner::Cell> cells_to_check = { m_end_cell };  // store cells to check, initialized to end
                                                                              // cell
 
     while (cells_to_check.size() > 0)
     {
-        WavefrontPlanner::Coord cell = cells_to_check.front();
+        WavefrontPlanner::Cell cell = cells_to_check.front();
         cells_to_check.pop_front();
+
+        if( cell.x == m_start_cell.x && cell.y == m_start_cell.y )
+            break;
 
         int cell_value = getCellValue(cell);
         auto adjacent_cells = getAdjacentCoords(cell);
+
+        if(adjacent_cells.size() < 1) {
+	    std::cout << "Create bug : " << cell.x << " " << cell.y <<  ". Value " << cell_value << std::endl;
+        }
 
         for (int i = 0; i < adjacent_cells.size(); i++)
         {
@@ -136,12 +233,12 @@ int* WavefrontPlanner::getMap()
     return m_WPA_map.data();
 }
 
-WavefrontPlanner::Coord WavefrontPlanner::getNextPathCell(const Coord& current_cell, bool direction_X)
+WavefrontPlanner::Cell WavefrontPlanner::getNextPathCell(const Cell& current_cell, bool direction_X)
 {
     int current_value = getCellValue(current_cell);
     
     auto adjacent_coords = getAdjacentCoords(current_cell);
-    std::vector<WavefrontPlanner::Coord> possible_cells;
+    std::vector<WavefrontPlanner::Cell> possible_cells;
 
 
     for(int i = 0; i < adjacent_coords.size(); i++) {
@@ -149,7 +246,7 @@ WavefrontPlanner::Coord WavefrontPlanner::getNextPathCell(const Coord& current_c
             continue;
         else {
             //we check value first
-            if(getCellValue(adjacent_coords[i]) > current_value)
+            if(getCellValue(adjacent_coords[i]) >= current_value)
                 continue;
             //if the cell is in the direction we are travelling, we use it
 	    if(adjacent_coords[i].x != current_cell.x && direction_X)
@@ -161,6 +258,7 @@ WavefrontPlanner::Coord WavefrontPlanner::getNextPathCell(const Coord& current_c
         }
     }
     if(possible_cells.size() < 1) {
+        std::cout << "Bug cell : " << current_cell.x << " " << current_cell.y <<std::endl;
 	throw std::logic_error("No path found");
     }
     //we return the first possible candidate, no priority
@@ -169,14 +267,15 @@ WavefrontPlanner::Coord WavefrontPlanner::getNextPathCell(const Coord& current_c
 
 void WavefrontPlanner::createPath()
 {
-    WavefrontPlanner::Coord current_cell = m_start_cell;
+    createMap();
+    WavefrontPlanner::Cell current_cell = m_start_cell;
     bool direction_X = true; //store if we are currently going on X axis
 
-    std::vector<WavefrontPlanner::Coord> path = {current_cell};
+    std::vector<WavefrontPlanner::Cell> path = {current_cell};
 
     while(getCellValue(current_cell) != 1) {
         /*while we are not at the end*/
-        WavefrontPlanner::Coord next_cell;
+        WavefrontPlanner::Cell next_cell;
         try {
             next_cell = getNextPathCell(current_cell, direction_X);
         } catch(std::logic_error) {
@@ -200,7 +299,15 @@ void WavefrontPlanner::createPath()
 
 }
 
+std::vector<WavefrontPlanner::Coord> WavefrontPlanner::getPath()
+{
+    std::vector<WavefrontPlanner::Coord> path;
+    for(int i = 0; i < m_path.size(); i++) {
+        path.push_back( mapToOdom(cellToCord( m_path[i] )) );
+    }
 
+    return path;
+}
 
 
 
--- src/WPA/WPA.hpp
+++ src/WPA/WPA.hpp
@@ -9,37 +9,49 @@
 class WavefrontPlanner
 {
   public:
-    struct Coord
+    struct Cell
     {
         int x, y;
     };
+    struct Coord
+    {
+        float x, y;
+    };
 
-    WavefrontPlanner(int width, int height, const std::vector<int8_t>& occupancy_grid);
+    WavefrontPlanner(int width, int height, float origin_x, float origin_y, float resolution, const std::vector<int8_t>& occupancy_grid);
     WavefrontPlanner(){};
-    void setStart(int x, int y);
-    void setEnd(int x, int y);
-    void createMap();
+    void setStart(float x, float y);
+    void setEnd(float x, float y);
     void createPath();
     int* getMap();
-    std::vector<Coord> getPath() { return m_path; };
+    std::vector<Coord> getPath();
 
   private:
     int m_width, m_height;
+    float m_resolution;
     std::vector<int8_t> m_occupancy_grid;
     std::vector<int> m_WPA_map;
-    std::vector<Coord> m_path;
+    std::vector<Cell> m_path;
 
-    Coord A[4];
-    Coord m_start_cell;
-    Coord m_end_cell;
+    Cell A[4];
+    Coord m_origin;
+    Cell m_start_cell;
+    Cell m_end_cell;
 
-    int8_t getOccupancyValue(Coord cell);
-    int getCellValue(Coord cell);
-    bool isPathableCell(Coord cell);
-    std::vector<Coord> getAdjacentCoords(const Coord& cell);
+    void init();
+    void createMap();
+    void flipOccupancyGrid();
+    int8_t getOccupancyValue(Cell cell);
+    int getCellValue(Cell cell);
+    Coord mapToOdom(Coord coord);
+    Coord odomToMap(Coord coord);
+    Cell coordToCell(Coord coord);
+    Coord cellToCord(Cell cell);
+    bool isPathableCell(Cell cell);
+    std::vector<Cell> getAdjacentCoords(const Cell& cell);
     void getCoordFromIndex(int cell_index, int& x, int& y);
-    void setCellValue(Coord cell, int value);
-    Coord getNextPathCell(const Coord& cell, bool direction_X);
+    void setCellValue(Cell cell, int value);
+    Cell getNextPathCell(const Cell& cell, bool direction_X);
 };
 
 #endif /* WPA_HPP */
